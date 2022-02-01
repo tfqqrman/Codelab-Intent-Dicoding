e1f95a2abdc8c45d8d2d86807142829f051d699e
tfqqrman <tfqqrman@gmail.com>
2022-02-01 21:21:10 +0700

    Innitial commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.example.myapplication
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val pindahActivity: Button = findViewById(R.id.btn_move_activity)
+        pindahActivity.setOnClickListener(this)
+
+        val pindahActivityData: Button = findViewById(R.id.btn_move_activity_data)
+        pindahActivityData.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        when(v?.id){
+            R.id.btn_move_activity ->{
+                val pindahIntent = Intent(this@MainActivity,ActivityPindah::class.java)
+                startActivity(pindahIntent)
+            }
+            R.id.btn_move_activity_data->{
+                val pindahIntentData = Intent(this@MainActivity,PindahDenganData::class.java)
+                pindahIntentData.putExtra(PindahDenganData.EXTRA_NAME,"Januar")
+                pindahIntentData.putExtra(PindahDenganData.EXTRA_AGE,90)
+                startActivity(pindahIntentData)
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/myapplication/PindahDenganData.kt
@@ -0,0 +1,23 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class PindahDenganData : AppCompatActivity() {
+    companion object{
+        const val EXTRA_AGE = "extra_age"
+        const val EXTRA_NAME = "extra_name"
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pindah_dengan_data)
+
+        val tvDataReceived:TextView = findViewById(R.id.tv_data_received)
+        val name = intent.getStringExtra(EXTRA_NAME)
+        val age = intent.getStringExtra(EXTRA_AGE)
+        val text = "hello my name is $name, I'm $age y.o"
+        tvDataReceived.text = text
+    }
+}
